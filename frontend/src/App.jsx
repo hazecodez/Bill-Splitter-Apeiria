2afd1cc0b03ff99ad6ebe6a8af5555fb0be41d73
hazecodez <muhammedhashir298@gmail.com>
2024-10-28 23:03:54 +0530

    Context done for theme change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./Context/ThemeContext";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" />
-        <Route path="/login"    />
-      </Routes>
-    </Router>
-  )
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/login" />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
---
+++ frontend/src/Context/ThemeContext.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const switchTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  const theme = isDarkMode ? "dark" : "light";
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [isDarkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, switchTheme }}>
+      {" "}
+      {children}{" "}
+    </ThemeContext.Provider>
+  );
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
